647d9faa3ee723d51aa0419843824354ad20f379
stenoritama <shr.nrht.0623@gmail.com>
2016-04-13 20:27:23 +0900

    add Extended Euclidean Algorithm

--- algebra/EuclideanAlgorithm.hs
+++ algebra/EuclideanAlgorithm.hs
@@ -1,6 +1,15 @@
+-- Euclidean Algorithm
 acpGcd :: Int -> Int -> Int
 acpGcd a b
-  | rmdr == 0 = b
-  | otherwise = acpGcd b rmdr
-    where rmdr = a `mod` b
+  | r == 0 = b
+  | otherwise = acpGcd b r
+    where r = a `mod` b
+          q = a `div` b
+
+-- Extended Euclidean Algorithm (find Bézout's identity)
+extendedGcd :: Int -> Int -> Int -> Int -> Int -> Int -> (Int, Int, Int)
+extendedGcd a b x1 x2 y1 y2
+  | r == 0 = (r, x2, y2)
+  | otherwise = extendedGcd b r x2 (x1 - q * x2) y2 (y1 - q * y2)
+    where r = a `mod` b
           q = a `div` b
